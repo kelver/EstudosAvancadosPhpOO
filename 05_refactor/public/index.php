8aa39650a5d2f4bfbed7f15bb57e9d0623cf9749
Kelver Machado <kelver_kmv@hotmail.com>
2017-02-25 19:53:38 -0400

    Teste de compartilhamento de serviços com pimple factory

--- 05_refactor/public/index.php
+++ 05_refactor/public/index.php
@@ -12,4 +12,20 @@ $container = new Container();
 $container['date'] = function (){
     return new \DateTime;
 };
+$container['dateFactory'] = $container->factory(function (){
+    return new \DateTime;
+});
+//testando compartilhamento de serviço sem factory
 var_dump($container['date']);
+echo "<br>";
+echo "<br>";
+sleep(2);
+var_dump($container['date']);
+echo "<br>";
+echo "<br>";
+//testando compartilhamento de serviço com factory
+var_dump($container['dateFactory']);
+sleep(2);
+echo "<br>";
+echo "<br>";
+var_dump($container['dateFactory']);
